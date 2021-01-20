ae69ac10f1e7270bea705d2bf4ebaeabba6caeb3
Dharmasheelan <dharmasheelan11@gmail.com>
2021-01-20 23:33:52 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+if(process.env.NODE_ENV !== "production") {
+    require("dotenv").config()
+}
+
+var express = require("express");
+var app = express();
+var expressLayouts = require("express-ejs-layouts");
+
+var indexRouter = require("./routes/index")
+
+ app.set("view engine", "ejs")
+ app.set("views", __dirname + "/views")
+ app.set("layout", "layouts/layout")
+
+ app.use(expressLayouts)
+ app.use(express.static("public"))
+
+
+ const mongoose = require("mongoose");
+ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser : true, useUnifiedTopology: true})
+ const db = mongoose.connection
+ db.on('error', error => console.log(error))
+ db.once('open', error => console.log("Connected Mongoose"))
+
+ app.use("/", indexRouter)
+
+ app.listen(process.env.PORT || 3000)
